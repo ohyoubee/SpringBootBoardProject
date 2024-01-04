15fca663cc3d0efe6f7d37aae0b493fef76f5dcd
ohyoubee <dhdbql1022@gmail.com>
2024-01-04 19:58:38 +0900

    BoardSerivce.java 에서 findAll 기능 추가 함 .

--- src/main/java/com/example/springbootboardproject/service/BoardSerivce.java
+++ src/main/java/com/example/springbootboardproject/service/BoardSerivce.java
@@ -1,9 +1,21 @@
 package com.example.springbootboardproject.service;
 
 
-import lombok.Setter;
+import com.example.springbootboardproject.entity.Board;
+import com.example.springbootboardproject.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class BoardSerivce {
+
+    private final BoardRepository boardRepository;
+
+
+    List<Board> findAll(){
+        return boardRepository.findAll();
+    }
 }
